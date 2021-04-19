8221e8fc5e88af8fae5cad4a21e47b955ae2d059
Heyday7 <kth3447@gmail.com>
2021-04-19 10:23:34 +0900

    board/views.py 작성

--- blog_back/board/views.py
+++ blog_back/board/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import *
+from .serializers import *
+
+
+class PostViewSet(viewsets.ModelViewSet):
+	queryset = Post.objects.all()
+	serializers = PostSerializer
+
+	def perform_create(self, serializer):
+		serializers.save(user=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+	queryset = Comment.objects.all()
+	serializers = CommentSerializer
+
+	def perform_create(self, serializer):
+		serializers.save(user=self.request.user, post=Post.objects.get(id=self.kwargs['post_id']))
+
+
+class SeriesViewSet(viewsets.ModelViewSet):
+	queryset = Series.objects.all()
+	serializers = SeriesSerializer
 
 # Create your views here.
