f4510b0dd9b556f9118c8f3c184d42b582b847e5
Heyday7 <kth3447@gmail.com>
2021-04-19 09:07:07 +0900

    indentation 수정

--- blog_back/board/models.py
+++ blog_back/board/models.py
@@ -2,54 +2,54 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
-  title = models.CharField(max_length=100)
-  content = models.TextField()
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
-  series = models.ForeignKey(
-    'Series', 
-    on_delete=models.SET_NULL,
-    related_names='posts',
-    related_query_name='post',
-    null=True)
-  user = models.ForeignKey(
-    User,
-    on_delete=models.CASCADE,
-    related_names='posts',
-    related_query_name='post'
-  )
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    series = models.ForeignKey(
+        'Series', 
+        on_delete=models.SET_NULL,
+        related_names='posts',
+        related_query_name='post',
+        null=True)
+    user = models.ForeignKey(
+      	User,
+		on_delete=models.CASCADE,
+		related_names='posts',
+		related_query_name='post'
+    )
 
-  def __str__(self):
-    return self.title
+    def __str__(self):
+      	return self.title
 
 
 class Comment(models.Model):
-  content = models.TextField()
-  created_at = models.DateTimeField(auto_now_add=True)
-  user = models.ForeignKey(
-    User,
-    on_delete=models.CASCADE,
-    related_name='comments',
-    related_query_name='comment' 
-  )
-  post = models.ForeignKey(
-    Post, 
-    on_delete=models.CASCADE,
-    related_name='comments',
-    related_query_name='comment' 
-  )
-  
-  def __str__(self):
-    return self.content
+	content = models.TextField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	user = models.ForeignKey(
+		User,
+		on_delete=models.CASCADE,
+		related_name='comments',
+		related_query_name='comment' 
+	)
+	post = models.ForeignKey(
+		Post, 
+		on_delete=models.CASCADE,
+		related_name='comments',
+		related_query_name='comment' 
+	)
+	
+	def __str__(self):
+		return self.content
 
 
 
 class Series(models.Model):
-  name = models.CharField(max_length=100, unique=True)
-  updated_at = models.DateTimeField(auto_now=True)
+	name = models.CharField(max_length=100, unique=True)
+	updated_at = models.DateTimeField(auto_now=True)
 
-  def __str__(self):
-    return self.name
+	def __str__(self):
+		return self.name
 
 
 # Create your models here.
