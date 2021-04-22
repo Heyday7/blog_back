04002409de33f4941f71386507fd0015801f8591
Heyday7 <kth3447@gmail.com>
2021-04-22 20:39:41 +0900

    board 관련 url 코드 작성

---
+++ blog_back/board/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework import routers
+from .views import *
+
+router = routers.DefaultRouter()
+router.register(r'post', PostViewSet)
+router.register(r'post/(?P<post_id>[0-9]+)/comment', CommentViewSet)
+router.register(r'series', SeriesViewSet)
+
+urlpatterns = [
+  path('', include(router.urls))
+]
